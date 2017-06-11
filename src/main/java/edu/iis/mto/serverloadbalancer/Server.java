eb73dd1af3f919836360f7b11224128264674408
patrykxk <patrix215@gmail.com>
2017-06-11 18:34:00 +0200

    filling server partially

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,9 +4,11 @@ package edu.iis.mto.serverloadbalancer;
 public class Server {
 
 	public double currentLoadPecentage;
+    public int capacity;
 
-	public Server(int capacity) {
-	}
+    public Server(int capacity) {
+	    this.capacity = capacity;
+    }
 
 	public boolean contains(Vm vm) {
 		return true;
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -32,7 +32,15 @@ public class ServerLoadBalancerTest {
 		assertThat(theServer, hasLoadPercentageOf(100.0d));
 		assertThat("the server should contain vm", theServer.contains(theVm));
 	}
+	@Test
+	public void balancingOneServerWithTenSlotCapacity_andOneSlotVm_fillsTheServerWithTenPercent() {
+		Server theServer = a(server().withCapacity(10));
+		Vm theVm = a(vm().ofSize(1));
+		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
+		assertThat(theServer, hasLoadPercentageOf(10.0d));
+		assertThat("the server should contain vm", theServer.contains(theVm));
+	}
 	private void balance(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers, vms);
 	}
