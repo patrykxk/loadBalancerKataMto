95dfef62ed8bbb2798514a5f560b56add75c1c87
patrykxk <patrix215@gmail.com>
2017-06-11 19:07:53 +0200

    adding more than one vms to server

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,21 +1,30 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
 
-    public static final double MAXIMAL_LOAD = 100.0d;
+    public static final double MAXIMUM_LOAD = 100.0d;
     public double currentLoadPercentage;
-	public int capacity;
+    private int capacity;
+    private List<Vm> vms = new ArrayList();
 
-	public Server(int capacity) {
-		this.capacity = capacity;
-	}
+    public Server(int capacity) {
+	    this.capacity = capacity;
+    }
 
 	public boolean contains(Vm vm) {
 		return true;
 	}
 
-    public void addVms(Vm[] vms) {
-        currentLoadPercentage = (double) vms[0].size / (double)this.capacity * MAXIMAL_LOAD;
+    public void addVms(Vm vm) {
+        currentLoadPercentage = (double)vm.size / (double) this.capacity * MAXIMUM_LOAD;
+        this.vms.add(vm);
+    }
+
+    public int countVms() {
+        return vms.size();
     }
 }
---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerVmsCountMatcher.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Created by Patryk on 2017-06-11.
+ */
+public class ServerVmsCountMatcher extends TypeSafeMatcher<Server> {
+    private final int expectedVmsCount;
+
+    public ServerVmsCountMatcher(int expectedVmsCount) {
+        this.expectedVmsCount = expectedVmsCount;
+    }
+
+    protected boolean matchesSafely(Server server) {
+        return expectedVmsCount == server.countVms();
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description mismatchDescription) {
+        mismatchDescription.appendText("Server vms count: ").appendValue(item.countVms());
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("Server vms count: ").appendValue(expectedVmsCount);
+    }
+}
